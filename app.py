b0eca23a780e2116683fdba3a0842e2e3b99b0f3
Iris Ineke <ieineke@nuc102.bin.bioinf.nl>
2025-01-14 11:26:35 +0100

    variabelen voorgesorteerd voor gebruiker + hist werkt nu ook

--- app.py
+++ app.py
@@ -28,26 +28,56 @@ def get_data():
     return pd.read_csv(data_path, header = 0)
 
 
-def widget_hist(data):
+# ! hier de lijst aanpassen
+def lists():
+    var_list = ('BIRTHYEAR', 'AGE_T1', 'AGE_T2', 'AGE_T3', 'ZIP_CODE',
+       'BMI_T1', 'WEIGHT_T1', 'HIP_T1', 'HEIGHT_T1', 'WAIST_T1', 'BMI_T2',
+       'WEIGHT_T2', 'HIP_T2', 'HEIGHT_T2', 'WAIST_T2', 'HEIGHT_T3',
+       'WEIGHT_T3', 'HIP_T3', 'WAIST_T3', 'FINANCE_T1', 'DBP_T1', 'DBP_T2',
+       'HBF_T1', 'HBF_T2', 'MAP_T1', 'MAP_T2', 'SBP_T1', 'SBP_T2', 'CHO_T1', 'GLU_T1', 'CHO_T2', 'GLU_T2',
+       'LLDS', 'SUMOFALCOHOL', 'SUMOFKCAL', 'MWK_VAL',
+       'SCOR_VAL', 'MWK_NO_VAL', 'SCOR_NO_VAL', 'PREGNANCIES', 'C_SUM_T1', 'A_SUM_T1',
+       'SC_SUM_T1', 'I_SUM_T1', 'E_SUM_T1', 'SD_SUM_T1', 'V_SUM_T1',
+       'D_SUM_T1',
+       'NSES_YEAR', 'NSES', 'NEIGHBOURHOOD1_T2', 'NEIGHBOURHOOD2_T2',
+       'NEIGHBOURHOOD3_T2', 'NEIGHBOURHOOD4_T2', 'NEIGHBOURHOOD5_T2',
+       'NEIGHBOURHOOD6_T2', 'MENTAL_DISORDER_T1', 'MENTAL_DISORDER_T2')
+    groupby_list = ('GENDER','EDUCATION_LOWER_T1',
+       'EDUCATION_LOWER_T2', 'WORK_T1', 'WORK_T2',
+       'LOW_QUALITY_OF_LIFE_T1', 'LOW_QUALITY_OF_LIFE_T2',
+       'HTN_MED_T1',
+       'RESPIRATORY_DISEASE_T1', 'SMOKING', 'METABOLIC_DISORDER_T1',
+       'METABOLIC_DISORDER_T2', 'SPORTS_T1',
+       'CYCLE_COMMUTE_T1', 'VOLUNTEER_T1', 'OSTEOARTHRITIS',
+       'BURNOUT_T1', 'DEPRESSION_T1', 'SLEEP_QUALITY', 'DIAG_CFS_CDC',
+       'DIAG_FIBROMYALGIA_ACR', 'DIAG_IBS_ROME3', 'NEIGHBOURHOOD1_T2', 'NEIGHBOURHOOD2_T2',
+       'NEIGHBOURHOOD3_T2', 'NEIGHBOURHOOD4_T2', 'NEIGHBOURHOOD5_T2',
+       'NEIGHBOURHOOD6_T2', 'MENTAL_DISORDER_T1', 'MENTAL_DISORDER_T2')
+    return var_list, groupby_list
+# bug: 'NEIGHBOURHOOD1_T2' --> legenda niet volledig in beeld
+# bug: scatterplot --> sorteert de legenda niet op volgorde
+
+
+def widget_hist(data, var_list, groupby_list):
     widget_hist_multi = pn.widgets.Select(
-        name = 'Histogram', options = list(data.columns))
+        name = 'Histogram', options = list(var_list))
     widget_groupby_hist = pn.widgets.Select(
-        name = 'Groupby', options = list(data.columns))
+        name = 'Groupby', options = list(groupby_list))
     return widget_hist_multi, widget_groupby_hist
 
 
 def histplot_body(data, widget_hist_multi, widget_groupby_hist):
-    histplot = data.hvplot.hist(y = widget_hist_multi, by = "SMOKING")
+    histplot = data.hvplot.hist(y = widget_hist_multi, by = widget_groupby_hist)
     return histplot
 
 
-def widget_scatter(data):
+def widget_scatter(data, var_list, groupby_list):
     widget_scatter_first = pn.widgets.Select(name = 'Scatterplot_y', 
-                                             options = list(data.columns))
+                                             options = list(var_list))
     widget_scatter_second = pn.widgets.Select(name = 'Scatterplot_x', 
-                                              options = list(data.columns))
+                                              options = list(var_list))
     widget_groupby_scat = pn.widgets.Select(name = 'groupby', 
-                                            options = list(data.columns))
+                                            options = list(groupby_list))
     return widget_scatter_first, widget_scatter_second, widget_groupby_scat
 
 
@@ -60,8 +90,9 @@ def scatterplot_body(data, widget_scatter_first, widget_scatter_second, widget_g
 
 def main():
     data = get_data()
-    widget_hist_multi, widget_groupby_hist = widget_hist(data)
-    widget_scatter_first, widget_scatter_second, widget_groupby_scat = widget_scatter(data)
+    var_list, groupby_list = lists()
+    widget_hist_multi, widget_groupby_hist = widget_hist(data, var_list, groupby_list)
+    widget_scatter_first, widget_scatter_second, widget_groupby_scat = widget_scatter(data, var_list, groupby_list)
 
     histogram = pn.bind(histplot_body, data, widget_hist_multi, widget_groupby_hist)
     scatterplot = pn.bind(scatterplot_body, data, widget_scatter_first, widget_scatter_second, widget_groupby_scat)
