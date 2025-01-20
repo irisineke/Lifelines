f8e11d6d81f83f154d09beae0191f9a9710e394f
Iris Ineke <ieineke@nuc102.bin.bioinf.nl>
2025-01-20 17:00:18 +0100

    -

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from configparser import ConfigParser
 import ast
 import pandas as pd
 import panel as pn
-import hvplot.pandas
+import hvplot
 
 
 pn.extension(design = "material", sizing_mode = "stretch_width")
