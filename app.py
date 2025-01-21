54911a16872ca5489deecfe94076a564baebd9d6
Iris Ineke <ieineke@nuc102.bin.bioinf.nl>
2025-01-21 21:34:45 +0100

    print statements eruit gehaald

--- app.py
+++ app.py
@@ -103,10 +103,7 @@ def widget_scatter(var_list, groupby_list):
     widget_groupby_scat = pn.widgets.Select(name = 'Sorteren op',
                                             options = list(groupby_list))
     switch_button = pn.widgets.Checkbox(name = 'Wissel assen')
-    print(type(switch_button))
-    # return widget_scatter_first, widget_scatter_second, widget_groupby_scat, switch_button
     return widget_scatter_first, widget_scatter_second, widget_groupby_scat, switch_button
-
 
 
 def scatterplot_body(data, widget_scatter_first, widget_scatter_second, widget_groupby_scat, switch_button):
@@ -119,16 +116,11 @@ def scatterplot_body(data, widget_scatter_first, widget_scatter_second, widget_g
     : return, scatterplot, een scatterplot van de ingevulde variabele
     '''
     if switch_button == True:
-        print('HEY HIJ IS TRUE!!')
         first_value = widget_scatter_first
         second_value = widget_scatter_second
         widget_scatter_first = second_value
         widget_scatter_second = first_value
-        # switch_button = False
 
-
-    # if switch_button == False:
-    #     print('HEY HIJ IS FALSE!!')
 
     scatterplot = data.hvplot.scatter(y = widget_scatter_first,
                                       x = widget_scatter_second,
